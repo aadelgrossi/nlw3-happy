9105af5027d840bd28d08f7be9bbf13311c6cede
Andre Grossi <iamandregrossi@gmail.com>
2020-10-13 01:24:05 -0300

    Finish Landing page styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import Head from 'next/head'
 
-import { Container } from '@/styles/pages/Landing'
+import { FiArrowRight } from 'react-icons/fi'
+import {
+  Container,
+  Wrapper,
+  LeftSection,
+  Location,
+  EnterAppButton
+} from '@/styles/pages/Landing'
 import Logo from '../assets/logo.svg'
 
 const Home: React.FC = () => {
@@ -11,7 +18,24 @@ const Home: React.FC = () => {
         <title>Happy</title>
       </Head>
 
-      <Logo />
+      <Wrapper>
+        <Logo />
+
+        <LeftSection>
+          <h1>Leve felicidade para o mundo</h1>
+
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </LeftSection>
+
+        <Location>
+          <strong>Paranavaí</strong>
+          <span>Paraná</span>
+        </Location>
+
+        <EnterAppButton href="">
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
+        </EnterAppButton>
+      </Wrapper>
     </Container>
   )
 }
--- src/styles/pages/Landing.ts
+++ src/styles/pages/Landing.ts
@@ -4,4 +4,73 @@ export const Container = styled.div`
   width: 100vw;
   height: 100vh;
   background: linear-gradient(329.54deg, #29b6d1 0%, #00c7c7 100%);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: space-between;
+  background: url('/images/landing-illustration.svg') no-repeat 80% center;
+
+  width: 100%;
+  max-width: 1100px;
+
+  height: 100%;
+  max-height: 680px;
+`
+export const LeftSection = styled.main`
+  h1 {
+    max-width: 350px;
+    font-size: 76px;
+    font-weight: 900;
+    line-height: 70px;
+  }
+
+  p {
+    max-width: 350px;
+    margin-top: 40px;
+    font-size: 24px;
+    line-height: 34px;
+  }
+`
+
+export const Location = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+
+  font-size: 24px;
+  line-height: 34px;
+  display: flex;
+  flex-direction: column;
+  text-align: right;
+
+  strong {
+    font-weight: 800;
+  }
+`
+export const EnterAppButton = styled.a`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+
+  width: 80px;
+  height: 80px;
+  background: #ffd666;
+  border-radius: 30px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background: #96feff;
+  }
 `
