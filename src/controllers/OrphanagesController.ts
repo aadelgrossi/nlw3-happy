c4edf32abf8f23ce564a4e643c3caf3606c07210
Andre Grossi <iamandregrossi@gmail.com>
2020-10-14 14:53:25 -0300

    Add show route

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -12,6 +12,16 @@ export default {
     return response.json(orphanages)
   },
 
+  async show(request: Request, response: Response) {
+    const { id } = request.params
+
+    const orphanagesRepository = getRepository(Orphanage)
+
+    const orphanages = await orphanagesRepository.findOneOrFail(id)
+
+    return response.json(orphanages)
+  },
+
   async create(request: Request, response: Response) {
     try {
       const {
--- src/routes/orphanage.routes.ts
+++ src/routes/orphanage.routes.ts
@@ -4,6 +4,7 @@ import OrphanagesController from '@controllers/OrphanagesController'
 const orphanageRouter = Router();
 
 orphanageRouter.get('/', OrphanagesController.index);
+orphanageRouter.get('/:id', OrphanagesController.show);
 orphanageRouter.post('/', OrphanagesController.create);
 
 export default orphanageRouter;
