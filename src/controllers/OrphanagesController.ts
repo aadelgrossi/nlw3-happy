e2954745e3e239478d94b03b888c0cd39aa23420
Andre Grossi <iamandregrossi@gmail.com>
2020-10-14 14:49:20 -0300

    Refactor logic to OrphanagesController

---
+++ src/controllers/OrphanagesController.ts
@@ -0,0 +1,38 @@
+import { getRepository } from 'typeorm'
+import Orphanage from '@models/Orphanage'
+
+import { Request, Response } from 'express'
+
+export default {
+  async create(request: Request, response: Response) {
+    try {
+      const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends } = request.body
+
+        const orphanagesRepository = getRepository(Orphanage)
+
+        const orphanage = orphanagesRepository.create({
+          name,
+          latitude,
+          longitude,
+          about,
+          instructions,
+          opening_hours,
+          open_on_weekends
+        })
+
+        await orphanagesRepository.save(orphanage)
+
+        return response.json(orphanage)
+
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  }
+}
--- src/routes/orphanage.routes.ts
+++ src/routes/orphanage.routes.ts
@@ -1,47 +1,9 @@
 import { Router } from 'express';
-import { getRepository } from 'typeorm'
-import Orphanage from '@models/Orphanage'
+import OrphanagesController from '@controllers/OrphanagesController'
 
 const orphanageRouter = Router();
 
-// orphanageRouter.get('/', (request, response) => {
-//   try {
-//     // TODO
-//   } catch (err) {
-//     return response.status(400).json({ error: err.message });
-//   }
-// });
-
-orphanageRouter.post('/', async (request, response) => {
-  try {
-    const {
-      name,
-      latitude,
-      longitude,
-      about,
-      instructions,
-      opening_hours,
-      open_on_weekends } = request.body
-
-      const orphanagesRepository = getRepository(Orphanage)
-
-      const orphanage = orphanagesRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-      })
-
-      await orphanagesRepository.save(orphanage)
-
-      return response.json(orphanage)
-
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
-});
+// orphanageRouter.get('/', OrphanagesController.get);
+orphanageRouter.post('/', OrphanagesController.create);
 
 export default orphanageRouter;
