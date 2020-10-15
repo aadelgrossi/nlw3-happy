f87aa337bf96ed04218b248ca65b89b8d37ea50f
Andre Grossi <iamandregrossi@gmail.com>
2020-10-15 00:46:23 -0300

    Install cors and yup validations

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import 'reflect-metadata';
 import express from 'express';
 import 'express-async-errors'
 import routes from './routes';
+import cors from 'cors'
 import morgan from 'morgan';
 import path from 'path'
 import './database';
 import errorHandler from './errors/handler';
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(
