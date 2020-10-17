98853f9acc4a88ceaf5c2c34f5c02f80c33c758d
Andre Grossi <iamandregrossi@gmail.com>
2020-10-17 01:24:15 -0300

    Update structure and styles for OrphanageDetails page

--- mobile/src/pages/OrphanageDetails/index.tsx
+++ mobile/src/pages/OrphanageDetails/index.tsx
@@ -1,12 +1,113 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+import { Marker } from 'react-native-maps'
+import mapMarkerImg from '../../images/mapmarker.png'
 
-import { Container } from './styles'
+import { Feather, FontAwesome } from '@expo/vector-icons'
+
+import {
+  Container,
+  ImagesContainer,
+  Image,
+  DetailsContainer,
+  Title,
+  Description,
+  MapContainer,
+  Map,
+  RoutesContainer,
+  RoutesText,
+  Separator,
+  ScheduleContainer,
+  BlueItem,
+  BlueText,
+  GreenItem,
+  GreenText,
+  ContactButton,
+  ContactButtonText
+} from './styles'
 
 const OrphanageDetails: React.FC = () => {
   return (
     <Container>
-      <Text>OrphanageDetails</Text>
+      <ImagesContainer>
+        <ScrollView horizontal pagingEnabled>
+          <Image
+            source={{
+              uri: 'https://fmnova.com.br/images/noticias/safe_image.jpg'
+            }}
+          />
+          <Image
+            source={{
+              uri: 'https://fmnova.com.br/images/noticias/safe_image.jpg'
+            }}
+          />
+          <Image
+            source={{
+              uri: 'https://fmnova.com.br/images/noticias/safe_image.jpg'
+            }}
+          />
+        </ScrollView>
+      </ImagesContainer>
+
+      <DetailsContainer>
+        <Title>Orfanato Esperança</Title>
+        <Description>
+          Presta assistência a crianças de 06 a 15 anos que se encontre em
+          situação de risco e/ou vulnerabilidade social.
+        </Description>
+
+        <MapContainer>
+          <Map
+            initialRegion={{
+              latitude: -27.2092052,
+              longitude: -49.6401092,
+              latitudeDelta: 0.008,
+              longitudeDelta: 0.008
+            }}
+            zoomEnabled={false}
+            pitchEnabled={false}
+            scrollEnabled={false}
+            rotateEnabled={false}
+          >
+            <Marker
+              icon={mapMarkerImg}
+              coordinate={{
+                latitude: -27.2092052,
+                longitude: -49.6401092
+              }}
+            ></Marker>
+          </Map>
+        </MapContainer>
+
+        <RoutesContainer>
+          <RoutesText>Ver rotas no Google Maps</RoutesText>
+        </RoutesContainer>
+
+        <Separator />
+
+        <Title>Instruções para visita</Title>
+        <Description>
+          Venha como se sentir a vontade e traga muito amor e paciência para
+          dar.
+        </Description>
+
+        <ScheduleContainer>
+          <BlueItem>
+            <Feather name="clock" size={40} color="#2AB5D1" />
+            <BlueText>Segunda à Sexta 8h às 18h</BlueText>
+          </BlueItem>
+
+          <GreenItem>
+            <Feather name="info" size={40} color="#39CC83" />
+            <GreenText>Atendemos fim de semana</GreenText>
+          </GreenItem>
+        </ScheduleContainer>
+
+        <ContactButton>
+          <FontAwesome name="whatsapp" size={24} color="#FFF" />
+          <ContactButtonText>Entrar em contato</ContactButtonText>
+        </ContactButton>
+      </DetailsContainer>
     </Container>
   )
 }
