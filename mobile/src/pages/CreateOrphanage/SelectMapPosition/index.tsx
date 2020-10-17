57858384886be1f2f553f3a2931f66c98cdf0e30
Andre Grossi <iamandregrossi@gmail.com>
2020-10-17 01:24:35 -0300

    Update structure and styles for SelectMapPosition

--- mobile/src/pages/CreateOrphanage/SelectMapPosition/index.tsx
+++ mobile/src/pages/CreateOrphanage/SelectMapPosition/index.tsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import { Text } from 'react-native'
+import { useNavigation } from '@react-navigation/core'
+import React, { useCallback } from 'react'
+import { Marker } from 'react-native-maps'
 
-import { Container } from './styles'
+import mapMarkerImg from '../../../images/mapmarker.png'
+
+import { Container, Map, NextButton, NextButtonText } from './styles'
 
 const SelectMapPosition: React.FC = () => {
+  const navigation = useNavigation()
+
+  const handleNextStep = useCallback(() => {
+    navigation.navigate('OrphanageData')
+  }, [])
+
   return (
     <Container>
-      <Text>SelectMapPosition</Text>
+      <Map
+        initialRegion={{
+          latitude: -27.2092052,
+          longitude: -49.6401092,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008
+        }}
+      >
+        <Marker
+          icon={mapMarkerImg}
+          coordinate={{ latitude: -27.2092052, longitude: -49.6401092 }}
+        />
+      </Map>
+
+      <NextButton onPress={handleNextStep}>
+        <NextButtonText>Próximo</NextButtonText>
+      </NextButton>
     </Container>
   )
 }
