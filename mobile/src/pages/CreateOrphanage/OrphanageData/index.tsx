5c5643582ac38ec27b6813860323fb925ef49d3f
Andre Grossi <iamandregrossi@gmail.com>
2020-10-17 01:23:17 -0300

    Update structure and styles for OrphanageData page

--- mobile/src/pages/CreateOrphanage/OrphanageData/index.tsx
+++ mobile/src/pages/CreateOrphanage/OrphanageData/index.tsx
@@ -1,12 +1,54 @@
 import React from 'react'
-import { Text } from 'react-native'
 
-import { Container } from './styles'
+import { Feather } from '@expo/vector-icons'
+
+import {
+  Container,
+  Title,
+  Input,
+  ImagesInput,
+  Label,
+  NextButton,
+  NextButtonText,
+  SwitchContainer
+} from './styles'
+import { Switch } from 'react-native'
 
 const OrphanageData: React.FC = () => {
   return (
-    <Container>
-      <Text>OrphanageData</Text>
+    <Container contentContainerStyle={{ padding: 24 }}>
+      <Title>Dados</Title>
+
+      <Label>Sobre</Label>
+      <Input multiline style={{ height: 110 }} />
+
+      <Label>WhatsApp</Label>
+      <Input />
+
+      <Label>Fotos</Label>
+      <ImagesInput onPress={() => {}}>
+        <Feather name="plus" size={24} color="#15B6D6" />
+      </ImagesInput>
+
+      <Title>Visitação</Title>
+
+      <Label>Instruções</Label>
+      <Input multiline style={{ height: 110 }} />
+
+      <Label>Horário de visitas</Label>
+      <Input />
+
+      <SwitchContainer>
+        <Label>Abre no final de semana?</Label>
+        <Switch
+          thumbColor="#fff"
+          trackColor={{ false: '#ccc', true: '#39CC83' }}
+        />
+      </SwitchContainer>
+
+      <NextButton onPress={() => {}}>
+        <NextButtonText>Cadastrar</NextButtonText>
+      </NextButton>
     </Container>
   )
 }
