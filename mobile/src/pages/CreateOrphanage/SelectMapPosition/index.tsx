50b45b13304494f88fcb67074d920d3f639b46c5
Andre Grossi <iamandregrossi@gmail.com>
2020-10-18 01:54:19 -0300

    Turn callbacks into regular functions

--- mobile/src/pages/CreateOrphanage/SelectMapPosition/index.tsx
+++ mobile/src/pages/CreateOrphanage/SelectMapPosition/index.tsx
@@ -1,37 +1,47 @@
 import { useNavigation } from '@react-navigation/core'
-import React, { useCallback } from 'react'
-import { Marker } from 'react-native-maps'
+import React, { useState } from 'react'
+import { MapEvent, Marker } from 'react-native-maps'
 
 import mapMarkerImg from '../../../images/mapmarker.png'
+import initialRegion from '../../../constants/initialRegion'
 
 import { Container, Map, NextButton, NextButtonText } from './styles'
 
 const SelectMapPosition: React.FC = () => {
   const navigation = useNavigation()
+  const [position, setPosition] = useState({
+    latitude: 0,
+    longitude: 0
+  })
 
-  const handleNextStep = useCallback(() => {
-    navigation.navigate('OrphanageData')
-  }, [])
+  const handleNextStep = () => {
+    navigation.navigate('OrphanageData', { position: position })
+  }
+
+  const handleSelectMapPosition = (event: MapEvent) => {
+    setPosition(event.nativeEvent.coordinate)
+  }
 
   return (
     <Container>
-      <Map
-        initialRegion={{
-          latitude: -27.2092052,
-          longitude: -49.6401092,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008
-        }}
-      >
-        <Marker
-          icon={mapMarkerImg}
-          coordinate={{ latitude: -27.2092052, longitude: -49.6401092 }}
-        />
+      <Map initialRegion={initialRegion} onPress={handleSelectMapPosition}>
+        {position.latitude !== 0 && (
+          <Marker
+            icon={mapMarkerImg}
+            onDragEnd={handleSelectMapPosition}
+            coordinate={{
+              latitude: position.latitude,
+              longitude: position.longitude
+            }}
+          />
+        )}
       </Map>
 
-      <NextButton onPress={handleNextStep}>
-        <NextButtonText>Próximo</NextButtonText>
-      </NextButton>
+      {position.latitude !== 0 && (
+        <NextButton onPress={handleNextStep}>
+          <NextButtonText>Próximo</NextButtonText>
+        </NextButton>
+      )}
     </Container>
   )
 }
