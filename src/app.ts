fb9659af525f17ae7c02808a1127f295fc0ee2fe
Andre Grossi <iamandregrossi@gmail.com>
2020-10-19 00:44:59 -0300

    Fix-on-save prettier

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,22 @@
-import 'reflect-metadata';
-import express from 'express';
+import 'reflect-metadata'
+import express from 'express'
 import 'express-async-errors'
-import routes from './routes';
+import routes from './routes'
 import cors from 'cors'
-import morgan from 'morgan';
+import morgan from 'morgan'
 import path from 'path'
-import './database';
-import errorHandler from './errors/handler';
+import './database'
+import errorHandler from './errors/handler'
 
-const app = express();
+const app = express()
 app.use(cors())
-app.use(express.json());
-app.use(morgan("dev"));
+app.use(express.json())
+app.use(morgan('dev'))
 app.use(
-  "/uploads",
-  express.static(path.join(__dirname, "..", "tmp", "uploads"))
-);
-app.use(routes);
+  '/uploads',
+  express.static(path.join(__dirname, '..', 'tmp', 'uploads'))
+)
+app.use(routes)
 app.use(errorHandler)
 
-export default app;
+export default app
--- src/errors/handler.ts
+++ src/errors/handler.ts
@@ -1,4 +1,4 @@
-import { ErrorRequestHandler } from "express";
+import { ErrorRequestHandler } from 'express'
 import { ValidationError } from 'yup'
 
 interface ValidationErrors {
@@ -7,18 +7,20 @@ interface ValidationErrors {
 
 const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
   if (error instanceof ValidationError) {
-    let errors: ValidationErrors = {}
+    const errors: ValidationErrors = {}
 
     error.inner.forEach(err => {
       errors[err.path] = err.errors
     })
 
-    return response.status(400).json({ message: 'Validation has failed', errors })
+    return response
+      .status(400)
+      .json({ message: 'Validation has failed', errors })
   }
 
   console.error(error)
 
-  return response.status(500).json({ message: 'Internal server error'})
+  return response.status(500).json({ message: 'Internal server error' })
 }
 
 export default errorHandler
