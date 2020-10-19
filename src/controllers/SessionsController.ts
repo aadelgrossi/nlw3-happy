71e3e79cfb7f1fe2c752637be06384db346184df
Andre Grossi <iamandregrossi@gmail.com>
2020-10-19 00:03:32 -0300

    Create session controller and authentication route (POST /sessions)

---
+++ src/controllers/SessionsController.ts
@@ -0,0 +1,34 @@
+import User from '@models/User'
+import userView from '@views/users_view'
+import { Request, Response } from 'express'
+import { getRepository } from 'typeorm'
+
+import authConfig from '@config/auth'
+import { compare } from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+export default {
+  async create(request: Request, response: Response): Promise<Response> {
+    const usersRepository = getRepository(User)
+    const { email, password } = request.body
+
+    const findUser = await usersRepository.findOne({ where: { email } })
+
+    if (!findUser) {
+      return response.sendStatus(401)
+    }
+
+    const isValidPassword = await compare(password, findUser.password)
+
+    if (!isValidPassword) {
+      return response.sendStatus(401)
+    }
+
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = jwt.sign({}, secret, { subject: findUser.id, expiresIn })
+
+    const user = userView.render(findUser)
+    return response.json({ user, token })
+  }
+}
---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+import SessionsController from '@controllers/SessionsController'
+
+const sessionsRouter = Router()
+
+sessionsRouter.post('/', SessionsController.create)
+
+export default sessionsRouter
