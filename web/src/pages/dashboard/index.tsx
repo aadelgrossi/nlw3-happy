f1f2b8fc34e9b9725b85ae59628a85745e561b0c
Andre Grossi <iamandregrossi@gmail.com>
2020-10-21 00:26:02 -0300

    Update and style Dashboard page

--- web/src/pages/dashboard/index.tsx
+++ web/src/pages/dashboard/index.tsx
@@ -1,8 +1,21 @@
+import Sidebar from '@/components/Sidebar'
 // import { useAuth } from '@/hooks/auth'
-import { useAuth } from '@/hooks/auth'
+import dynamic from 'next/dynamic'
+import Link from 'next/link'
 import React from 'react'
+import { FiEdit3, FiTrash } from 'react-icons/fi'
 
-import { Container } from './styles'
+import {
+  Container,
+  Contents,
+  Header,
+  Separator,
+  OrphanagesContainer,
+  OrphanageDetails,
+  Item,
+  Button,
+  ButtonGroup
+} from './styles'
 
 interface User {
   id: string
@@ -14,14 +27,184 @@ interface UserProps {
   user: User
 }
 
+const MapWithNoSSR = dynamic(() => import('@/components/Map'), {
+  ssr: false
+})
+const MarkerWithNoSSR = dynamic(() => import('@/components/Marker'), {
+  ssr: false
+})
+
 const Dashboard: React.FC<UserProps> = () => {
-  const { isAuthenticated, user } = useAuth()
+  // const { isAuthenticated, user } = useAuth()
 
   return (
     <Container>
-      <h1>dashboard</h1>
+      <Sidebar />
+      {/* <h1>dashboard</h1>
       <p>Estou autenticado? {isAuthenticated ? 'Sim' : 'não'}</p>
-      {user && user.email}
+      {user && user.email} */}
+      <Contents>
+        <Header>
+          <h1>Orfanatos cadastrados</h1>
+          <span>2 orfanatos cadastrados</span>
+        </Header>
+        <Separator />
+
+        <OrphanagesContainer>
+          <Item>
+            <MapWithNoSSR
+              center={[-23.0878701, -52.4666419]}
+              zoom={15}
+              style={{
+                width: '100%',
+                height: 240,
+                border: '1px solid #DDE3F0',
+                borderRadius: 20
+              }}
+              dragging={false}
+              touchZoom={false}
+              zoomControl={false}
+              scrollWheelZoom={false}
+              doubleClickZoom={false}
+            >
+              <MarkerWithNoSSR
+                name="orfanato"
+                interactive={false}
+                position={[-23.0878701, -52.4666419]}
+              />
+            </MapWithNoSSR>
+            <OrphanageDetails>
+              <h2>Orfanato Esperança</h2>
+              <ButtonGroup>
+                <Link href="/orphanages">
+                  <Button>
+                    <FiEdit3 size={24} color="#15C3D6"></FiEdit3>
+                  </Button>
+                </Link>
+                <Link href="/orphanages/delete">
+                  <Button>
+                    <FiTrash size={24} color="#15C3D6"></FiTrash>
+                  </Button>
+                </Link>
+              </ButtonGroup>
+            </OrphanageDetails>
+          </Item>
+          <Item>
+            <MapWithNoSSR
+              center={[-23.0878701, -52.4666419]}
+              zoom={15}
+              style={{
+                width: '100%',
+                height: 240,
+                border: '1px solid #DDE3F0',
+                borderRadius: 20
+              }}
+              dragging={false}
+              touchZoom={false}
+              zoomControl={false}
+              scrollWheelZoom={false}
+              doubleClickZoom={false}
+            >
+              <MarkerWithNoSSR
+                name="orfanato"
+                interactive={false}
+                position={[-23.0878701, -52.4666419]}
+              />
+            </MapWithNoSSR>
+            <OrphanageDetails>
+              <h2>Orfanato Esperança</h2>
+              <ButtonGroup>
+                <Link href="/orphanages">
+                  <Button>
+                    <FiEdit3 size={24} color="#15C3D6"></FiEdit3>
+                  </Button>
+                </Link>
+                <Link href="/orphanages/delete">
+                  <Button>
+                    <FiTrash size={24} color="#15C3D6"></FiTrash>
+                  </Button>
+                </Link>
+              </ButtonGroup>
+            </OrphanageDetails>
+          </Item>
+          <Item>
+            <MapWithNoSSR
+              center={[-23.0878701, -52.4666419]}
+              zoom={15}
+              style={{
+                width: '100%',
+                height: 240,
+                border: '1px solid #DDE3F0',
+                borderRadius: 20
+              }}
+              dragging={false}
+              touchZoom={false}
+              zoomControl={false}
+              scrollWheelZoom={false}
+              doubleClickZoom={false}
+            >
+              <MarkerWithNoSSR
+                name="orfanato"
+                interactive={false}
+                position={[-23.0878701, -52.4666419]}
+              />
+            </MapWithNoSSR>
+            <OrphanageDetails>
+              <h2>Orfanato Esperança</h2>
+              <ButtonGroup>
+                <Link href="/orphanages">
+                  <Button>
+                    <FiEdit3 size={24} color="#15C3D6"></FiEdit3>
+                  </Button>
+                </Link>
+                <Link href="/orphanages/delete">
+                  <Button>
+                    <FiTrash size={24} color="#15C3D6"></FiTrash>
+                  </Button>
+                </Link>
+              </ButtonGroup>
+            </OrphanageDetails>
+          </Item>
+          <Item>
+            <MapWithNoSSR
+              center={[-23.0878701, -52.4666419]}
+              zoom={15}
+              style={{
+                width: '100%',
+                height: 240,
+                border: '1px solid #DDE3F0',
+                borderRadius: 20
+              }}
+              dragging={false}
+              touchZoom={false}
+              zoomControl={false}
+              scrollWheelZoom={false}
+              doubleClickZoom={false}
+            >
+              <MarkerWithNoSSR
+                name="orfanato"
+                interactive={false}
+                position={[-23.0878701, -52.4666419]}
+              />
+            </MapWithNoSSR>
+            <OrphanageDetails>
+              <h2>Orfanato Esperança</h2>
+              <ButtonGroup>
+                <Link href="/orphanages">
+                  <Button>
+                    <FiEdit3 size={24} color="#15C3D6"></FiEdit3>
+                  </Button>
+                </Link>
+                <Link href="/orphanages/delete">
+                  <Button>
+                    <FiTrash size={24} color="#15C3D6"></FiTrash>
+                  </Button>
+                </Link>
+              </ButtonGroup>
+            </OrphanageDetails>
+          </Item>
+        </OrphanagesContainer>
+      </Contents>
     </Container>
   )
 }
--- web/src/pages/dashboard/styles.ts
+++ web/src/pages/dashboard/styles.ts
@@ -3,3 +3,89 @@ import styled from 'styled-components'
 export const Container = styled.div`
   color: #000;
 `
+
+export const Contents = styled.div`
+  padding-top: 64px;
+  margin-left: 180px;
+  margin-right: 64px;
+`
+export const Header = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  h1 {
+    color: #4d6f80;
+  }
+
+  span {
+    color: #8fa7b2;
+  }
+`
+
+export const Separator = styled.hr`
+  width: 100%;
+  height: 1px;
+  border: 0;
+  background: #d3e2e6;
+  margin: 24px 0 40px;
+`
+export const OrphanagesContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: -32px;
+`
+export const Item = styled.div`
+  width: max(500px, 25vw);
+  background-color: #fff;
+  height: 320px;
+  border-radius: 20px;
+  border: 1px solid #d3e2e5;
+
+  margin-left: 32px;
+  margin-bottom: 32px;
+`
+
+export const OrphanageDetails = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 16px 32px;
+
+  h2 {
+    font-size: 24px;
+    color: #4d6f80;
+    font-weight: 700;
+  }
+`
+
+export const ButtonGroup = styled.div`
+  display: flex;
+  margin-left: auto;
+`
+
+export const Button = styled.a`
+  width: 48px;
+  height: 48px;
+  border-radius: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #ebf2f5;
+
+  transition: background-color 0.2s ease-in;
+
+  & + & {
+    margin-left: 8px;
+  }
+
+  &:hover {
+    background-color: #15c3d6;
+
+    svg {
+      stroke: #fff;
+    }
+  }
+`
