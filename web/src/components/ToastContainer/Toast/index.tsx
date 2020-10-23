a000f5c9f851b80c083c76914631febd7a27dcfd
Andre Grossi <iamandregrossi@gmail.com>
2020-10-23 00:52:54 -0300

    Create ToastContainer, Toast components and useToast hook

---
+++ web/src/components/ToastContainer/Toast/index.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react'
+import { FiCheckCircle, FiInfo, FiXCircle } from 'react-icons/fi'
+import { AiOutlineStop } from 'react-icons/ai'
+
+import { ToastMessage, useToast } from '../../../hooks/toast'
+import { Container } from './styles'
+
+interface ToastProps {
+  message: ToastMessage
+  style: Record<string, unknown>
+}
+
+const icons = {
+  info: <FiInfo size={24} />,
+  error: <AiOutlineStop size={24} />,
+  success: <FiCheckCircle size={24} />
+}
+
+const Toast: React.FC<ToastProps> = ({ message, style }) => {
+  const { removeToast } = useToast()
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(message.id)
+    }, 3000)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [message.id, removeToast])
+
+  return (
+    <Container
+      style={style}
+      type={message.type}
+      hasDescription={Number(!!message.description)}
+    >
+      {icons[message.type || 'info']}
+      <div>
+        <strong>{message.title}</strong>
+        {message.description && <p>{message.description}</p>}
+      </div>
+
+      <button onClick={() => removeToast(message.id)} type="button">
+        <FiXCircle size={18} />
+      </button>
+    </Container>
+  )
+}
+
+export default Toast
---
+++ web/src/components/ToastContainer/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  position: absolute;
+  right: 4%;
+  top: 2%;
+  padding: 24px;
+  overflow: hidden;
+`
