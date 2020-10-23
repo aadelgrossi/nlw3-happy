ab155588366c6e0ab7178cf179c81898bd3aa472
Andre Grossi <iamandregrossi@gmail.com>
2020-10-23 13:21:08 -0300

    Rearrange routes for orphanage router

--- src/routes/orphanage.routes.ts
+++ src/routes/orphanage.routes.ts
@@ -9,15 +9,15 @@ import ensureAuthenticated from '~/middlewares/ensureAuthenticated'
 const orphanageRouter = Router()
 const upload = multer(uploadConfig.multer)
 
-orphanageRouter.get('/', OrphanagesController.index)
-orphanageRouter.get('/:id', OrphanagesController.show)
-orphanageRouter.post('/', upload.array('images'), OrphanagesController.create)
-
-orphanageRouter.get('/pending', ApprovalController.pending)
+orphanageRouter.get('/pending', ensureAuthenticated, ApprovalController.pending)
 orphanageRouter.put(
   '/:id/approve',
   ensureAuthenticated,
   ApprovalController.approve
 )
+
+orphanageRouter.get('/', OrphanagesController.index)
+orphanageRouter.get('/:id', OrphanagesController.show)
+orphanageRouter.post('/', upload.array('images'), OrphanagesController.create)
 
 export default orphanageRouter
