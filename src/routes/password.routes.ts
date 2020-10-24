894ba6f1745a71d632c4984b038a1319c2257ce3
Andre Grossi <iamandregrossi@gmail.com>
2020-10-24 00:05:20 -0300

    Remove old files

--- src/routes/password.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import ResetPasswordController from '~/controllers/ResetPasswordController'
-
-const forgotPasswordRouter = Router()
-
-forgotPasswordRouter.post('/forgot', ResetPasswordController.create)
-forgotPasswordRouter.put('/reset', ResetPasswordController.reset)
-
-export default forgotPasswordRouter
--- src/routes/sessions.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import SessionsController from '~/controllers/SessionsController'
-
-const sessionsRouter = Router()
-
-sessionsRouter.post('/', SessionsController.create)
-
-export default sessionsRouter
