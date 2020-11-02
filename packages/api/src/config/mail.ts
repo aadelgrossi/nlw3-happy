a2bde763bf014f20be0c5ce4a8c321214bc974c0
Andre Grossi <iamandregrossi@gmail.com>
2020-11-02 00:59:54 -0300

    Configure SES Mail Provider

--- packages/api/src/config/mail.ts
+++ packages/api/src/config/mail.ts
@@ -1,7 +1,19 @@
 interface MailConfig {
   driver: 'ethereal' | 'ses'
+  defaults: {
+    from: {
+      name: string
+      email: string
+    }
+  }
 }
 
 export default {
-  driver: process.env.MAIL_DRIVER || 'ethereal'
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+  defaults: {
+    from: {
+      name: 'Andre Grossi',
+      email: 'info@andregrossi.com'
+    }
+  }
 } as MailConfig
--- packages/api/src/providers/MailProvider/index.ts
+++ packages/api/src/providers/MailProvider/index.ts
@@ -1,13 +1,13 @@
 import { container } from 'tsyringe'
 
 import EtherealMailProvider from './implementations/EtherealMailProvider'
-// import SESMailProvider from './implementations/SESMailProvider'
+import SESMailProvider from './implementations/SESMailProvider'
 import IMailProvider from './models/IMailProvider'
 import mailConfig from '~/config/mail'
 
 const providers = {
-  ethereal: container.resolve(EtherealMailProvider)
-  //   ses: container.resolve(SESMailProvider)
+  ethereal: container.resolve(EtherealMailProvider),
+  ses: container.resolve(SESMailProvider)
 }
 
 container.registerInstance<IMailProvider>(
