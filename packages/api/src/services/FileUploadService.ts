c8ca2ea80f47c1f7e08bfbda50a7d315f9d1969b
Andre Grossi <iamandregrossi@gmail.com>
2020-11-14 02:34:41 -0300

    Create FileUploadService and call in controller on create

---
+++ packages/api/src/services/FileUploadService.ts
@@ -0,0 +1,21 @@
+import { injectable, inject } from 'tsyringe'
+
+import IStorageProvider from '~/providers/StorageProvider/models/IStorageProvider'
+
+interface IFileRequest {
+  filename: string
+}
+
+@injectable()
+class FileUploadService {
+  constructor(
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider
+  ) {}
+
+  public async execute({ filename }: IFileRequest): Promise<string> {
+    return await this.storageProvider.saveFile(filename)
+  }
+}
+
+export default FileUploadService
