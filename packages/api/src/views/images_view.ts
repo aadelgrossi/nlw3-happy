5f5a071ca2eeb20af8285482302ed29135eda133
Andre Grossi <iamandregrossi@gmail.com>
2020-11-14 02:35:23 -0300

    Update serializer to return custom url depending on storage provider

--- packages/api/src/views/images_view.ts
+++ packages/api/src/views/images_view.ts
@@ -1,3 +1,4 @@
+import uploadConfig from '~/config/upload'
 import Image from '~/models/Image'
 
 interface ImageOutput {
@@ -5,11 +6,18 @@ interface ImageOutput {
   url: string
 }
 
+const urlFormatter = {
+  disk: (name: string) =>
+    `${process.env.API_URL}/uploads/${encodeURIComponent(name)}`,
+  s3: (name: string) =>
+    `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${name}`
+}
+
 export default {
   render(image: Image): ImageOutput {
     return {
       id: image.id,
-      url: `${process.env.API_URL}/uploads/${image.path}`
+      url: urlFormatter[uploadConfig.driver](image.path)
     }
   },
 
