473f43dc13b7dc8249c39eeeebd5aadb71302fca
Andre Grossi <iamandregrossi@gmail.com>
2020-12-08 02:54:16 -0300

    Add conditional rendering of onboarding screen; render navigator only after fetching value from async storage (restored is true)

--- packages/mobile/src/routes.tsx
+++ packages/mobile/src/routes.tsx
@@ -1,58 +1,68 @@
 import React from 'react'
+
 import { NavigationContainer } from '@react-navigation/native'
 import {
   CardStyleInterpolators,
   createStackNavigator
 } from '@react-navigation/stack'
+import { usePersistStorage } from 'react-native-use-persist-storage'
 
-import Map from './pages/Map'
-import OrphanageDetails from './pages/OrphanageDetails'
-
-import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition'
-import OrphanageData from './pages/CreateOrphanage/OrphanageData'
 import Header from './components/Header'
+import OrphanageData from './pages/CreateOrphanage/OrphanageData'
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition'
+import Map from './pages/Map'
+import Onboarding from './pages/Onboarding'
+import OrphanageDetails from './pages/OrphanageDetails'
 
 const { Navigator, Screen } = createStackNavigator()
 
 const Routes: React.FC = () => {
+  const [hasRunBefore, _, restored] = usePersistStorage<string>(
+    'Happy@HasRunBefore',
+    null
+  )
+
   return (
-    <NavigationContainer>
-      <Navigator
-        screenOptions={{
-          headerShown: false,
-          cardStyle: {
-            backgroundColor: '#f2f3f5'
-          },
-          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS
-        }}
-      >
-        <Screen name="OrphanagesMap" component={Map}></Screen>
-        <Screen
-          name="OrphanageDetails"
-          component={OrphanageDetails}
-          options={{
-            headerShown: true,
-            header: () => <Header showDismiss={false} title="Orfanato" />
+    restored && (
+      <NavigationContainer>
+        <Navigator
+          screenOptions={{
+            headerShown: false,
+            cardStyle: {
+              backgroundColor: '#f2f3f5'
+            },
+            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS
           }}
-        ></Screen>
-        <Screen
-          name="SelectMapPosition"
-          component={SelectMapPosition}
-          options={{
-            headerShown: true,
-            header: () => <Header title="Selecione no mapa" />
-          }}
-        ></Screen>
-        <Screen
-          name="OrphanageData"
-          component={OrphanageData}
-          options={{
-            headerShown: true,
-            header: () => <Header title="Preencha os dados" />
-          }}
-        ></Screen>
-      </Navigator>
-    </NavigationContainer>
+        >
+          {!hasRunBefore && <Screen name="Onboarding" component={Onboarding} />}
+          <Screen name="OrphanagesMap" component={Map} />
+          <Screen
+            name="OrphanageDetails"
+            component={OrphanageDetails}
+            options={{
+              headerShown: true,
+              header: () => <Header showDismiss={false} title="Orfanato" />
+            }}
+          />
+          <Screen
+            name="SelectMapPosition"
+            component={SelectMapPosition}
+            options={{
+              headerShown: true,
+              header: () => <Header title="Selecione no mapa" />
+            }}
+          />
+          <Screen
+            name="OrphanageData"
+            component={OrphanageData}
+            options={{
+              headerShown: true,
+              header: () => <Header title="Preencha os dados" />
+            }}
+          />
+        </Navigator>
+      </NavigationContainer>
+    )
   )
 }
 
