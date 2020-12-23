ef67ca164e8dd584c11725973b9e30df15493320
Andre Grossi <iamandregrossi@gmail.com>
2020-12-22 22:35:41 -0300

    Fix server.ts to show port number correctly

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,5 +1,5 @@
 import app from './app'
 
 app.listen(process.env.PORT || 3333, () => {
-  console.log(`🚀 Server started on port ${process.env || 3333}!`)
+  console.log(`🚀 Server started on port ${process.env.PORT || 3333}!`)
 })
