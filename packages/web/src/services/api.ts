630828f8b9bdf9a19c8258ea8e62fde453658e92
Andre Grossi <iamandregrossi@gmail.com>
2020-12-24 01:53:21 -0300

    Add basic axios interceptor to capture a failing response

--- packages/web/src/services/api.ts
+++ packages/web/src/services/api.ts
@@ -5,4 +5,17 @@ const api = axios.create({
   withCredentials: true
 })
 
+api.interceptors.response.use(
+  response => {
+    return response
+  },
+  error => {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    return Promise.reject({
+      statusCode: error.response.status,
+      data: error.response.data
+    })
+  }
+)
+
 export default api
