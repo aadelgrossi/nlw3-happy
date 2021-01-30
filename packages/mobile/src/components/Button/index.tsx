ee4e215f7e72fcf35e4d33420faa8529d5f69784
Andre Grossi <iamandregrossi@gmail.com>
2021-01-30 11:37:51 -0300

    Add activity/loading indicator for button

--- packages/mobile/src/components/Button/index.tsx
+++ packages/mobile/src/components/Button/index.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
 
+import { ActivityIndicator } from 'react-native'
 import { RectButtonProperties } from 'react-native-gesture-handler'
 
 import { Container, ButtonText } from './styles'
 
 interface ButtonProps extends RectButtonProperties {
   confirm?: boolean
+  loading?: boolean
   onPress(e: any): void
 }
 
-export const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  loading = false,
+  ...props
+}) => {
   return (
     <Container {...props}>
-      <ButtonText>{children}</ButtonText>
+      {loading ? (
+        <ActivityIndicator size="small" color="#fff" />
+      ) : (
+        <ButtonText>{children}</ButtonText>
+      )}
     </Container>
   )
 }
