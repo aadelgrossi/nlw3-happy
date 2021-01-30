a96847c7f264d44cb30df2d561f12f29afa6380f
Andre Grossi <iamandregrossi@gmail.com>
2021-01-30 19:40:31 -0300

    Update SectionTwo form with new SegmentedControl, slugify orphanage name as image title and fix submit callback

--- packages/mobile/src/pages/CreateOrphanage/DataSectionTwo/index.tsx
+++ packages/mobile/src/pages/CreateOrphanage/DataSectionTwo/index.tsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 
 import { yupResolver } from '@hookform/resolvers/yup'
 import { CompositeNavigationProp, RouteProp } from '@react-navigation/core'
 import { StackNavigationProp } from '@react-navigation/stack'
 import { useForm } from 'react-hook-form'
-import { Switch } from 'react-native'
+import slugify from 'slugify'
 
-import { Button, Input } from '~/components'
+import {
+  Button,
+  Input,
+  SegmentedControl,
+  SegmentedControlOption
+} from '~/components'
 import {
   CreateOrphanageParamList,
   InitialRoutesParamList
@@ -14,10 +19,9 @@ import {
 import api from '~/services/api'
 
 import {
-  Container,
   Title,
   Label,
-  SwitchContainer,
+  Form,
   LabelWrapper,
   TitleWrapper,
   StepItemText,
@@ -25,6 +29,7 @@ import {
 } from '../styles'
 import { OrphanageSectionTwoFormData } from '../types'
 import schema from './schema'
+import { Container } from './styles'
 
 interface DataSectionTwoProps {
   navigation: CompositeNavigationProp<
@@ -40,7 +45,7 @@ export const DataSectionTwo: React.FC<DataSectionTwoProps> = ({
   },
   navigation
 }) => {
-  const [openOnWeekends, setOpenOnWeekends] = useState(false)
+  const [openOnWeekends, setOpenOnWeekends] = useState(true)
 
   const {
     control,
@@ -50,80 +55,92 @@ export const DataSectionTwo: React.FC<DataSectionTwoProps> = ({
   } = useForm<OrphanageSectionTwoFormData>({
     defaultValues: {
       instructions: '',
-      opening_hours: '',
-      open_on_weekends: false
+      opening_hours: ''
     },
     resolver: yupResolver(schema)
   })
 
-  const createOrphanage = async (data: OrphanageSectionTwoFormData) => {
-    const { latitude, longitude, name, about, images } = orphanage
-    const formData = new FormData()
+  const createOrphanage = useCallback(
+    async ({ instructions, opening_hours }: OrphanageSectionTwoFormData) => {
+      const { name, latitude, longitude, about, images, whatsapp } = orphanage
+      const data = new FormData()
 
-    formData.append('name', name)
-    formData.append('about', about)
-    formData.append('instructions', data.instructions)
-    formData.append('opening_hours', data.opening_hours)
-    formData.append('latitude', String(latitude))
-    formData.append('longitude', String(longitude))
-    formData.append('open_on_weekends', String(openOnWeekends))
+      data.append('name', name)
+      data.append('about', about)
+      data.append('instructions', instructions)
+      data.append('whatsapp', whatsapp)
+      data.append('opening_hours', opening_hours)
+      data.append('latitude', String(latitude))
+      data.append('longitude', String(longitude))
+      data.append('open_on_weekends', String(openOnWeekends))
 
-    images.forEach((image, index) => {
-      formData.append('images', {
-        name: `image_${index}.jpg`,
-        type: 'image/jpeg',
-        uri: image
-      } as any)
-    })
-
-    api
-      .post('/orphanages', data)
-      .then(() => {
-        // Toast.show('Orfanato cadastrado com sucesso.')
-        alert('sucesso')
-        // confirm
-        navigation.navigate('OrphanagesMap')
+      images.forEach((image, index) => {
+        data.append('files', {
+          name: `${slugify(name, {
+            lower: true
+          })}-${index + 1}.jpg`,
+          type: 'image/jpeg',
+          uri: image
+        } as any)
       })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+      api
+        .post('/orphanages', data)
+        .then(() => {
+          alert('sucesso')
+          navigation.navigate('OrphanagesMap')
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    },
+    [orphanage, openOnWeekends, navigation]
+  )
+
+  const toggleOpenOnWeekends = useCallback(() => {
+    setOpenOnWeekends(state => !state)
+  }, [])
 
   return (
     <Container>
-      <TitleWrapper>
-        <Title>Visitação</Title>
-        <LabelWrapper>
-          <StepItemText>01 - </StepItemText>
-          <CurrentStep>02</CurrentStep>
-        </LabelWrapper>
-      </TitleWrapper>
+      <Form>
+        <TitleWrapper>
+          <Title>Visitação</Title>
+          <LabelWrapper>
+            <StepItemText>01 - </StepItemText>
+            <CurrentStep>02</CurrentStep>
+          </LabelWrapper>
+        </TitleWrapper>
 
-      <Label>Instruções</Label>
-      <Input
-        name="instructions"
-        control={control}
-        multiline
-        style={{ height: 110 }}
-        errors={errors.instructions}
-      />
-
-      <Label>Horário de visitas</Label>
-      <Input
-        name="opening_hours"
-        control={control}
-        errors={errors.opening_hours}
-      />
-
-      <SwitchContainer>
-        <Label>Abre no final de semana?</Label>
-        <Switch
-          thumbColor="#fff"
-          trackColor={{ false: '#ccc', true: '#39CC83' }}
-          value={openOnWeekends}
-          onValueChange={setOpenOnWeekends}
+        <Label>Instruções</Label>
+        <Input
+          name="instructions"
+          control={control}
+          multiline
+          style={{ height: 110 }}
+          errors={errors.instructions}
         />
-      </SwitchContainer>
+
+        <Label>Horário de visitas</Label>
+        <Input
+          name="opening_hours"
+          control={control}
+          errors={errors.opening_hours}
+        />
+
+        <Label>Atende final de semana?</Label>
+        <SegmentedControl>
+          <SegmentedControlOption
+            label="Sim"
+            active={openOnWeekends}
+            onPress={toggleOpenOnWeekends}
+          />
+          <SegmentedControlOption
+            label="Não"
+            active={!openOnWeekends}
+            onPress={toggleOpenOnWeekends}
+          />
+        </SegmentedControl>
+      </Form>
 
       <Button
         onPress={handleSubmit(createOrphanage)}
