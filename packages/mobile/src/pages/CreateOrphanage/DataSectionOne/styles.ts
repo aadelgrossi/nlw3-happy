5433463f9e1362af5538ee47a42446a7f2acb520
Andre Grossi <iamandregrossi@gmail.com>
2021-01-30 11:41:19 -0300

    Remove old ImageInput styles in DataSectionOne

--- packages/mobile/src/pages/CreateOrphanage/DataSectionOne/styles.ts
+++ packages/mobile/src/pages/CreateOrphanage/DataSectionOne/styles.ts
@@ -2,18 +2,6 @@ import { Feather } from '@expo/vector-icons'
 import { BorderlessButton } from 'react-native-gesture-handler'
 import styled from 'styled-components/native'
 
-export const ImagesInput = styled.TouchableOpacity`
-  background-color: rgba(255, 255, 255, 0.5);
-  border-style: dashed;
-  border-color: #96d2f0;
-  border-width: 1.4px;
-  border-radius: 20px;
-  height: 56px;
-  justify-content: center;
-  align-items: center;
-  margin: 8px 0 32px;
-`
-
 export const UploadedImagesContainer = styled.View``
 
 export const RemoveImage = styled(BorderlessButton)`
